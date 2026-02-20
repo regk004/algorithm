7fb6178222f749681f7f35d344c9c844261b2d38
regk004 <daniel040918@gmail.com>
2026-02-21 02:11:42 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string ab;
+    cin >> ab;
+
+    if (ab == "NLCS") cout << "North London Collegiate School";
+    else if (ab == "BHA") cout << "Branksome Hall Asia";
+    else if (ab == "KIS") cout << "Korea International School";
+    else if (ab == "SJA") cout << "St. Johnsbury Academy";
+}
+
